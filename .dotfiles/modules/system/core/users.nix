8426eab9d392cc7c5f3e0efa1b6c44361e6d7d8c
qzgpm <dlvn3jai@gmail.com>
2026-06-16 19:07:59 +0000

    will work i think

--- .dotfiles/modules/system/core/users.nix
+++ .dotfiles/modules/system/core/users.nix
@@ -3,5 +3,6 @@
     isNormalUser = true;
     shell = pkgs.zsh;
     extraGroups = ["wheel"];
+    hashedPassword= "$y$j9T$wffwRE9g2A/ucqM5SbXRNX.$0qYa7s.zMHhyiXZ9GW.RIuGACazGg34dniqS.PGX7C2";
   };
 }
